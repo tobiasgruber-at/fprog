a955a35aeaeb1881f054037b971f54cc827a660d
tobiasgruber <tgruber.at@gmail.com>
2022-11-22 16:53:27 +0100

    fix(6a): finish a.1

--- src/Angabe6.hs
+++ src/Angabe6.hs
@@ -15,23 +15,26 @@ newtype EUR  = EUR { euro :: Nat1 }
 data Skonto  = Kein_Skonto 
                | DreiProzent  
                | FuenfProzent 
-               | ZehnProzent
+               | ZehnProzent deriving (Eq,Show)
 
-data Waschmaschine    = M1 | M2 | M3 | M4 | M5
-data Waeschetrockner  = T1 | T2 | T3 | T4
-data Waescheschleuder = S1 | S2 | S3
+data Waschmaschine    = M1 | M2 | M3 | M4 | M5 deriving (Eq,Show,Enum)
+data Waeschetrockner  = T1 | T2 | T3 | T4 deriving (Eq,Show,Enum)
+data Waescheschleuder = S1 | S2 | S3 deriving (Eq,Show,Enum)
 
 data Typ = M Waschmaschine
            | T Waeschetrockner
-           | S Waescheschleuder
+           | S Waescheschleuder deriving (Eq,Show)
 
-data Quartal       = Q1 | Q2 | Q3 | Q4 deriving (Eq,Ord,Show)
-data Jahr          = J2023 | J2024 | J2025
+data Quartal       = Q1 | Q2 | Q3 | Q4 deriving (Eq,Ord,Show,Enum)
+data Jahr          = J2023 | J2024 | J2025 deriving (Show,Ord,Eq,Enum)
 data Lieferfenster = LF { quartal :: Quartal,
                           jahr    :: Jahr 
-                        }
+                        } deriving (Eq,Show)
 
 newtype Lieferausblick = LA (Lieferfenster -> Nat0)
+
+instance Eq Lieferausblick where
+  l1 == l2 = (wg_la l1) == (wg_la l2)
 
 data Datensatz 
    = DS { preis_in_euro :: Nat1,
@@ -39,11 +42,25 @@ data Datensatz
           lieferbare_stueckzahl_im_Zeitfenster :: Lieferausblick,
           skonto :: Skonto
         }
-     | Nicht_im_Sortiment
+     | Nicht_im_Sortiment deriving (Eq)
+
+instance Show Datensatz where
+  show DS { 
+    preis_in_euro = p, 
+    sofort_lieferbare_stueckzahl = s, 
+    lieferbare_stueckzahl_im_Zeitfenster = l, 
+    skonto = sk } = "{ preis: " ++ (show p) ++ ", sofort lieferb. stk: " ++ (show s) ++ ", skonto: " ++ (show sk) ++ "}"
+  show Nicht_im_Sortiment = "nicht im sortiment" 
 
 newtype Sortiment = Sort (Typ -> Datensatz)
 
-data Haendler = H1 | H2 | H3 | H4 | H5 | H6 | H7 | H8 | H9 | H10
+instance Show Sortiment where
+ show s = show (wg_so s)
+
+instance Eq Sortiment where
+  (Sort s1) == (Sort s2) = [s1 x | x <- t_liste] == [s2 x | x <- t_liste]
+
+data Haendler = H1 | H2 | H3 | H4 | H5 | H6 | H7 | H8 | H9 | H10 deriving (Eq,Ord,Show,Enum)
 
 newtype Anbieter = A (Haendler -> Sortiment)
 
@@ -61,12 +78,23 @@ class Wgf a where                -- Wgf fuer `wohlgeformt'
 
 -- Aufgabe A.1
 
+f_liste :: [Lieferfenster]
+f_liste = [(LF q j) | q <- [Q1 .. Q4], j <- [J2023 .. J2025]]
+
+t_liste :: [Typ]
+t_liste = [M w | w <- [M1 .. M5]] ++ [T t | t <- [T1 .. T4]] ++ [S s | s <- [S1 .. S3]] 
+
+h_liste :: [Haendler]
+h_liste = [H1 .. H10]
+
 wg_la :: Lieferausblick -> [(Lieferfenster,Nat0)]
+wg_la (LA l) = [(f, l f) | f <- f_liste] 
 
 wg_so :: Sortiment -> [(Typ,Datensatz)]
+wg_so (Sort s) = [(t, s t) | t <- t_liste]
 
-wg_ab :: Anbieter ->  [(Haendler,Sortiment)
-
+wg_ab :: Anbieter -> [(Haendler,Sortiment)]
+wg_ab (A a) = [(h, a h) | h <- h_liste]
 
 {- Knapp, aber gut nachvollziehbar gehen die Implementierungen folgendermassen vor:
    ...
@@ -75,14 +103,14 @@ wg_ab :: Anbieter ->  [(Haendler,Sortiment)
 
 -- Aufgabe A.2
 
-instance Wgf Lieferausblick where
- ist_wgf = error "Noch nicht implementiert!"
+--instance Wgf Lieferausblick where
+-- ist_wgf = error "Noch nicht implementiert!"
 
-instance Wgf Sortiment where
- ist_wgf = error "Noch nicht implementiert!"
+--instance Wgf Sortiment where
+-- ist_wgf = error "Noch nicht implementiert!"
 
-instance Wgf Anbieter where
-  ist_wgf = error "Noch nicht implementiert!"
+--instance Wgf Anbieter where
+--  ist_wgf = error "Noch nicht implementiert!"
 
 {- Knapp, aber gut nachvollziehbar gehen die Instanzbildungen fuer Wgf folgendermassen vor:
    ...
@@ -91,10 +119,10 @@ instance Wgf Anbieter where
 
 -- Aufgabe A.5
 
-type Haendlerliste = [Haendler]
+--type Haendlerliste = [Haendler]
 
-sofort_lieferfaehig :: Suchanfrage -> Anbieter -> Haendlerliste
-sofort_lieferfaehig _ _ = error "Noch nicht implementiert!"
+--sofort_lieferfaehig :: Suchanfrage -> Anbieter -> Haendlerliste
+--sofort_lieferfaehig _ _ = error "Noch nicht implementiert!"
 
 {- Knapp, aber gut nachvollziehbar geht die Implementierung folgendermassen vor:
    ...
@@ -103,11 +131,11 @@ sofort_lieferfaehig _ _ = error "Noch nicht implementiert!"
 
 -- Aufgabe A.6
 
-type Stueckzahl  = Nat0
-type Gesamtpreis = Nat0
+--type Stueckzahl  = Nat0
+--type Gesamtpreis = Nat0
  
-sofort_erhaeltliche_Stueckzahl :: Suchanfrage -> Anbieter -> (Stueckzahl,Gesamtpreis)
-sofort_erhaeltliche_Stueckzahl _ _ = error "Noch nicht implementiert!"
+--sofort_erhaeltliche_Stueckzahl :: Suchanfrage -> Anbieter -> (Stueckzahl,Gesamtpreis)
+--sofort_erhaeltliche_Stueckzahl _ _ = error "Noch nicht implementiert!"
 
 {- Knapp, aber gut nachvollziehbar geht die Implementierung folgendermassen vor:
    ...
@@ -116,9 +144,9 @@ sofort_erhaeltliche_Stueckzahl _ _ = error "Noch nicht implementiert!"
 
 -- Aufgabe A.7
 
-type Preis = EUR
-guenstigste_Lieferanten :: Suchanfrage -> Lieferfenster -> Anbieter -> Maybe Haendlerliste
-guenstigste_Lieferanten _ _ = error "Noch nicht implementiert!"
+--type Preis = EUR
+--guenstigste_Lieferanten :: Suchanfrage -> Lieferfenster -> Anbieter -> Maybe Haendlerliste
+--guenstigste_Lieferanten _ _ = error "Noch nicht implementiert!"
 
 {- Knapp, aber gut nachvollziehbar geht die Implementierung folgendermassen vor:
    ...
@@ -127,10 +155,10 @@ guenstigste_Lieferanten _ _ = error "Noch nicht implementiert!"
 
 -- Aufgabe A.8
 
-type RabattierterPreis = EUR
+--type RabattierterPreis = EUR
 
-guenstigste_Lieferanten_im_Lieferfenster :: Suchanfrage -> Lieferfenster -> Stueckzahl -> Anbieter -> [(Haendler,RabattierterPreis)]
-guenstigste_Lieferanten_im_Lieferfenster _ _ = error "Noch nicht implementiert!"
+--guenstigste_Lieferanten_im_Lieferfenster :: Suchanfrage -> Lieferfenster -> Stueckzahl -> Anbieter -> [(Haendler,RabattierterPreis)]
+--guenstigste_Lieferanten_im_Lieferfenster _ _ = error "Noch nicht implementiert!"
 
 {- Knapp, aber gut nachvollziehbar geht die Implementierung folgendermassen vor:
    ...
--- test/TestSuite6.hs
+++ test/TestSuite6.hs
@@ -1,37 +1,87 @@
 module TestSuite6 where
 
-import Test.Tasty
-import Test.Tasty.QuickCheck as QC
-import Test.Tasty.HUnit
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import qualified Control.Exception as Exc
 
-import Data.List
-import Data.Ord
+import           Angabe6
 
+-- check if error thrown
+expectSomeError :: String -> a -> Assertion
+expectSomeError msg val = do
+  res <- Exc.try (Exc.evaluate val)
+  case res of
+    Left (Exc.ErrorCall m) -> if m == msg then pure () else assertFailure ("Expected error '" ++ msg ++ "' but got error '" ++ m ++ "'") 
+    Right _ -> assertFailure "Expected error call but got none"
+
+
+-- Check if two lists contain same items
+equals x y = length x == length y && contains x y && contains y x 
+contains :: (Eq a) => [a] -> [a] -> Bool
+contains [] y = True
+contains (x:xs) y = elem x y && contains xs y
+ 
+-- Test data
+f_l :: [Lieferfenster]
+f_l = [(LF q j) | q <- [Q1 .. Q4], j <- [J2023 .. J2025]]
+t_l :: [Typ]
+t_l = [M w | w <- [M1 .. M5]] ++ [T t | t <- [T1 .. T4]] ++ [S s | s <- [S1 .. S3]] 
+h_l :: [Haendler]
+h_l = [H1 .. H10]
+
+la_fn_1 :: Lieferfenster -> Nat0
+la_fn_1 (LF q j) = if (j >= J2024) then 200 else 100
+la_fn_2 :: Lieferfenster -> Nat0
+la_fn_2 (LF q j) = if (q >= Q3) then 200 else 100
+
+ds_fn_1 :: Typ -> Datensatz 
+ds_fn_1 _ = (DS 1 1 (LA la_fn_1) Kein_Skonto)
+ds_fn_2 :: Typ -> Datensatz 
+ds_fn_2 _ = (DS 1 1 (LA la_fn_2) Kein_Skonto)
+
+ab_fn_1 :: Haendler -> Sortiment
+ab_fn_1 _ = (Sort ds_fn_1)
+ab_fn_2 :: Haendler -> Sortiment
+ab_fn_2 h = if h > H5 then (Sort ds_fn_2) else (Sort ds_fn_1) 
+
+-- Tests
 spec :: TestTree
-spec = testGroup "Angabe6" [ properties, unitTests]
-
-properties :: TestTree
-properties = testGroup "Properties" [qcProps]
-
-
-unitTests :: TestTree
-unitTests = testGroup "Unit tests"
-  [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
-
-  -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
+spec = testGroup "Angabe6" [
+    wgTests
   ]
-
-qcProps :: TestTree
-qcProps = testGroup "(checked by QuickCheck)"
-  [ QC.testProperty "sort == sort . reverse" $
-      \list -> sort (list :: [Int]) == sort (reverse list)
-  , QC.testProperty "Fermat's little theorem" $
-      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
-  -- the following property does not hold
-  , QC.testProperty "Fermat's last theorem" $
-      \x y z n ->
-        (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
-  ]
+ 
+wgTests :: TestTree
+wgTests =
+  testGroup
+    "Wertegraph Tests"
+    [ testCase "Lieferausblick Wertegraph 1" $
+      equals 
+      (wg_la (LA la_fn_1)) 
+      [(x, la_fn_1 x) | x <- f_l]
+      @?= True 
+    , testCase "Lieferausblick Wertegraph 2" $
+      equals
+      (wg_la (LA la_fn_2))
+      [(x, la_fn_2 x) | x <- f_l]
+      @?= True
+    , testCase "Sortiment Wertegraph 1" $
+      equals
+      (wg_so (Sort ds_fn_1))
+      [(x, ds_fn_1 x) | x <- t_l]
+      @?= True
+    , testCase "Sortiment Wertegraph 2" $
+      equals
+      (wg_so (Sort ds_fn_2))
+      [(x, ds_fn_2 x) | x <- t_l]
+      @?= True
+    , testCase "Anbieter Wertegraph 1" $
+      equals
+      (wg_ab (A ab_fn_1))
+      [(x, ab_fn_1 x) | x <- h_l]
+      @?= True
+    , testCase "Anbieter Wertegraph 2" $
+      equals
+      (wg_ab (A ab_fn_2))
+      [(x, ab_fn_2 x) | x <- h_l]
+      @?= True
+    ]
